58370e7ba7f0749cb5b70c15d0a5c1d8a1ce2f2d
martinem <martinem@gmx.com.mx>
2017-06-06 10:56:54 -0500

    Se agrega mantenimiento en Recordatorio Siniestros

---
+++ WS_GMX/Reaseguro/Recordatorios/Siniestros/IRecSiniestros.vb
@@ -0,0 +1,15 @@
+﻿Imports System.ServiceModel
+
+' NOTE: You can use the "Rename" command on the context menu to change the interface name "IRecSiniestros" in both code and config file together.
+<ServiceContract()>
+Public Interface IRecSiniestros
+
+    <OperationContract()>
+    Function ActualizaFaseRecup(id_Fase As Int32, strFase As String) As List(Of Nullable(Of Int32))
+
+    <OperationContract()>
+    Function InsertaFaseRecup(strFase As String) As List(Of Nullable(Of Int32))
+
+    <OperationContract()>
+    Function EliminaFaseRecup(id_Fase As Int32) As List(Of Nullable(Of Int32))
+End Interface
---
+++ WS_GMX/Reaseguro/Recordatorios/Siniestros/RecSiniestros.svc.vb
@@ -0,0 +1,37 @@
+﻿' NOTE: You can use the "Rename" command on the context menu to change the class name "RecSiniestros" in code, svc and config file together.
+' NOTE: In order to launch WCF Test Client for testing this service, please select RecSiniestros.svc or RecSiniestros.svc.vb at the Solution Explorer and start debugging.
+Imports WS_GMX
+
+Public Class RecSiniestros
+    Implements IRecSiniestros
+    Public db As New GMXEntities
+
+    Public Function ActualizaFaseRecup(id_Fase As Int32, strFase As String) As List(Of Nullable(Of Int32)) Implements IRecSiniestros.ActualizaFaseRecup
+        Dim Resultado As IList = Nothing
+        Try
+            Resultado = db.spU_FaseRecupera(id_Fase, strFase).ToList
+        Catch ex As Exception
+            Return Nothing
+        End Try
+        Return Resultado
+    End Function
+    Public Function InsertaFaseRecup(strFase As String) As List(Of Nullable(Of Int32)) Implements IRecSiniestros.InsertaFaseRecup
+        Dim Resultado As IList = Nothing
+        Try
+            Resultado = db.spI_FaseRecupera(strFase).ToList
+        Catch ex As Exception
+            Return Nothing
+        End Try
+        Return Resultado
+    End Function
+    Public Function EliminaFaseRecup(id_Fase As Int32) As List(Of Nullable(Of Int32)) Implements IRecSiniestros.EliminaFaseRecup
+        Dim Resultado As IList = Nothing
+        Try
+            Resultado = db.spD_FaseRecupera(id_Fase).ToList
+        Catch ex As Exception
+            Return Nothing
+        End Try
+        Return Resultado
+    End Function
+
+End Class
