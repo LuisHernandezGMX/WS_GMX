1ffb319d301d2894e40de0f09aab6db9d070412f
OscarSandovalGMX <oscar.sandoval@gmx.com.mx>
2017-07-17 10:12:02 -0500

    Filtros adicionales en busqueda de Orden de Pago

--- WS_GMX/Reaseguro/OrdenPago/IOrdenPago.vb
+++ WS_GMX/Reaseguro/OrdenPago/IOrdenPago.vb
@@ -19,5 +19,6 @@ Public Interface IOrdenPago
     Function ObtieneOrdenPago(nro_orden As String, Optional ByVal FiltroBrokerCia As String = "", Optional ByVal FiltroContrato As String = "", Optional ByVal FiltroPoliza As String = "",
                               Optional ByVal FiltroFecPago As String = "", Optional ByVal FiltroRamoCont As String = "", Optional ByVal FiltroUsuario As String = "", Optional ByVal FiltroEstatus As String = "",
                               Optional ByVal FiltroFecGen As String = "", Optional ByVal cod_moneda As Integer = -1, Optional ByVal FiltroAseg As String = "", Optional ByVal FiltroMonto As String = "",
+                              Optional ByVal FitroNatOP As String = "", Optional ByVal intFirmas As Integer = -1,
                               Optional ByVal CodUsuPermiso As String = "") As List(Of spS_OrdenPago_Result)
 End Interface
